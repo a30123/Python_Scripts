81a0ff3c77effe5089c6f0a0c56d0366b1136cb6
Yu-Ru Huang <mary@linshunghuang.com>
2015-04-22 17:29:38 +0800

    update

--- catch_data_unreal.py
+++ catch_data_unreal.py
@@ -101,6 +101,26 @@ def extract_serial_number(filename):
     serial_number_string=serial_number_string.replace('_','') 
     value_of_number=int(serial_number_string)
     return value_of_number
+    
+def write_array_to_csv(filename_path,listname):
+    import csv
+     
+    runnumberfile=open(filename_path,'w',newline='')
+    wr=csv.writer(runnumberfile,quoting=csv.QUOTE_ALL)
+    if type(listname)==list:
+        for item in listname:
+            wr.writerow([item])
+    elif type(listname)==np.ndarray:
+        if len(listname.shape)==1:
+            for item in listname:
+                wr.writerow([item])
+        else:
+            for item in listname:
+                wr.writerow(item)
+    else:
+        print("the structure you are writing is neither a list nor an np.ndarray")
+		
+    runnumberfile.close()
 #######################################
 
 
@@ -109,12 +129,12 @@ def extract_serial_number(filename):
 #########################################################################################################
 
 #intialize "sensor variable of interest","folder to accesss", and "folder to save output to"
-sensor_variables="Vent.vac"#-------------------------------------"sensor variable of interest"
-folder_to_read_from="E://MovedFromD//CSV//TS1//Vac_2363runs//setpoint"#--------------------------------------------"folder to access"
-sensor_variables2="TMAl_1.source"#-------------------------------------"sensor variable of interest"
-folder_to_read_from2="E://MovedFromD//CSV//TS1//MO1group_2363runs//deviation"#--------------------------------------------"folder to access"
-path_to_save_figures="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150422_VentVacWithTMAl//Output"#----------------------------"folder to save output to"
-
+sensor_variables="TMAl_1.source"#-------------------------------------"sensor variable of interest"
+folder_to_read_from="E://MovedFromD//CSV//TS1//MO1group_2363runs//setpoint"#--------------------------------------------"folder to access"
+folder_to_read_from2="E://MovedFromD//CSV//TS1//MO1group_2363runs//current"#--------------------------------------------"folder to access"
+folder_to_read_from3="E://MovedFromD//CSV//TS1//MO1group_2363runs//deviation"
+path_to_save_list="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150422_catch_data_unreal//Event_unreal.csv"#----------------------------"folder to save output to"
+PhysMax=500
 
 #########################################################################################################
 #######################################   MAIN PROGRAM        ###########################################
@@ -124,38 +144,35 @@ path_to_save_figures="C://Users//A30123.ITRI//Documents//Python Scripts//New_for
 files_in_folder = os.listdir(folder_to_read_from) 
 files_in_folder.sort(key=extract_serial_number)
 
+event_list=[]
 for i in range(len(files_in_folder)):
     #--------------------------------------------------------------------------------file path name for single csv file
     
     temp_file_name=files_in_folder[i]    
     single_file_path=os.path.join(folder_to_read_from, temp_file_name)
-    single_file_path2=os.path.join(folder_to_read_from2, temp_file_name.replace('-setpoint','-deviation'))
+    single_file_path2=os.path.join(folder_to_read_from2, temp_file_name.replace('-setpoint','-current'))
+    single_file_path3=os.path.join(folder_to_read_from3, temp_file_name.replace('-setpoint','-deviation'))
 
     #--------------------------------------------------------------------------------prints serial number
     serial_number=extract_serial_number(files_in_folder[i])
     print('Reading CSV file:',serial_number)
 
     #--------------------------------------------------------------------------------reads values from csv file of specified sensor variable
-    values_read_from_file=read_single_variable_as_stringlist_csv(single_file_path,sensor_variables)
-    run_length=len(values_read_from_file)    
+    setpoint_values=read_single_variable_as_stringlist_csv(single_file_path,sensor_variables)
+    current_values=read_single_variable_as_stringlist_csv(single_file_path2,sensor_variables)
+    deviation_values=read_single_variable_as_stringlist_csv(single_file_path3,sensor_variables)
     
-    diff_values_from_file=values_read_from_file[1:]-values_read_from_file[:-1]    
     
-    yes_no_negative_one=(diff_values_from_file==(-1))
-    if (sum(yes_no_negative_one)>0):
-        just_increments=np.array(range(len(diff_values_from_file)))
-        position_of_negative_ones=just_increments[yes_no_negative_one]
-        last_negative_one=max(position_of_negative_ones)        
-        plot_left_endpoint=max(0,(last_negative_one-15))
-        plot_right_endpoint=min((run_length-1),(last_negative_one+15))
+     
+    
+    calculated_deviation=current_values-setpoint_values    
+    
+    boolean_value=(abs(calculated_deviation-deviation_values*PhysMax/100)<1)
+    
+    if (sum(boolean_value)>100):
+        event_list.append(1)
     else:
-        plot_left_endpoint=0
-        plot_right_endpoint=(run_length-1)
-    
-    second_set_of_values=read_single_variable_as_stringlist_csv(single_file_path2,sensor_variables2)
-
+        event_list.append(0)
     #----------------------------------------------------------------------------------plots the values and saves as png file into designated folder    
-    plot_and_save_list_values_cooler(values_read_from_file,second_set_of_values,plot_left_endpoint,plot_right_endpoint,path_to_save_figures,str(serial_number)+".csv")
-    #plot_and_save_list_values(values_read_from_file,path_to_save_figures,files_in_folder[i])
-    
-
+     
+write_array_to_csv(path_to_save_list,event_list)
