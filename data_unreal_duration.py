301fbb638eaca345c7e0df00e99eb8e6a7e930d4
Yu-Ru Huang <mary@linshunghuang.com>
2015-04-24 16:45:46 +0800

    add comment

--- data_unreal_duration.py
+++ data_unreal_duration.py
@@ -103,7 +103,7 @@ def extract_serial_number(filename):
     return value_of_number
     
 def write_array_to_csv(filename_path,listname):
-    import csv
+#    import csv
      
     runnumberfile=open(filename_path,'w',newline='')
     wr=csv.writer(runnumberfile,quoting=csv.QUOTE_ALL)
