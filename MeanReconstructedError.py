2556eda7c5224344a96345d6a1452613b50ad4e8
Yu-Ru Huang <mary@linshunghuang.com>
2015-05-04 17:25:25 +0800

    reconstructed error features

--- MeanReconstructedError.py
+++ MeanReconstructedError.py
@@ -60,50 +60,38 @@ def write_array_to_csv(filename_path,listname):
     
 def extract_serial_number(filename):
 #    import re
-    extract_regular_expression=re.search('(^\d+_relative)',filename)
+    extract_regular_expression=re.search('(^\d+_reconstructed)',filename)
     serial_number_string=extract_regular_expression.group(0)
-    serial_number_string=serial_number_string.replace('_relative','')
+    serial_number_string=serial_number_string.replace('_reconstructed','')
  
     value_of_number=int(serial_number_string)
     return value_of_number
 #########################################################################################################
 #######################################   INITIALIZING        ###########################################
 #########################################################################################################
-folder_to_read_from="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150430_TMAl_1_source_relative_error//TMAl_source_relative_error_CSV"
-folder_to_read_from2="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150504_TMAl_source_setpoint_partition_rewritten//Output//CSV"
-output_folder_filename="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150504_feature_mean_relative_error//mean_relative_error_flat_region.csv"
+folder_to_read_from="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150504_TMAl_1_source_reconstructed_error//TMAl_reconstructed_error_CSV"
+output_folder_filename="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150504_TMAl_source_mean_reconstructed_error//mean_reconstructed_error.csv"
 #########################################################################################################
 #######################################   MAIN PROGRAM        ###########################################
 #########################################################################################################
 tstart = time.time()
 files_in_folder = os.listdir(folder_to_read_from) 
 files_in_folder.sort(key=extract_serial_number)
-mean_relative_error_list=[]
+mean_reconstructed_error_list=[]
 for i in range(len(files_in_folder)):
-    temp_file_name=files_in_folder[i]   
-    serial_number=extract_serial_number(files_in_folder[i])
-    temp_file_name2=str(serial_number)+'.csv'    
+    temp_file_name=files_in_folder[i]    
     print(temp_file_name)
     single_file_path=os.path.join(folder_to_read_from, temp_file_name)
-    single_file_path2=os.path.join(folder_to_read_from2, temp_file_name2)
  
-    relative_error_values=get_single_column_from_csv(single_file_path)
-    category_values=get_single_column_from_csv(single_file_path2)
-    category_values_float=np.array(category_values,dtype='float16')
+    reconstructed_error_values=get_single_column_from_csv(single_file_path)
     
-    flat_region_occurrence=(category_values_float==4)    
-    
-    if (sum(flat_region_occurrence)!=0):
-        relative_error_float=np.array(relative_error_values,dtype='float16')
-        relative_error_to_consider=relative_error_float[flat_region_occurrence]
-        
-        mean_relative_error_float=np.mean(relative_error_to_consider)
-        mean_relative_error_list.append(mean_relative_error_float)
-    else:
-        mean_relative_error_list.append(-0.5)
+    reconstructed_error_float=abs(np.array(reconstructed_error_values,dtype='float16'))
+    mean_reconstructed_error_float=np.mean(reconstructed_error_float)
+ 
+    mean_reconstructed_error_list.append(mean_reconstructed_error_float)
 
 
-write_array_to_csv(output_folder_filename,mean_relative_error_list)
+write_array_to_csv(output_folder_filename,mean_reconstructed_error_list)
 
 
 print('RUN TIME: %.2f secs' % (time.time()-tstart))
