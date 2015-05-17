6f0ed71c072fa7adc9922be2abeb17a7cd696bbd
Yu-Ru Huang <mary@linshunghuang.com>
2015-05-17 18:10:52 +0800

    important

--- cluster_category_proportions.py
+++ cluster_category_proportions.py
@@ -18,6 +18,7 @@ Created on Sat May 16 21:52:27 2015
 #########################################################################################################
 import time
 import numpy as np
+import csv
 #import re
 #import math
 import matplotlib.pyplot as plt
@@ -40,16 +41,43 @@ def get_matrix_from_csv(csvpathfilename):
             thelist.append(row)
         
     return np.array(thelist)    
+    
+
+    
+def write_array_to_csv(filename_path,listname):
+    import csv
+     
+    runnumberfile=open(filename_path,'w',newline='')
+    wr=csv.writer(runnumberfile,quoting=csv.QUOTE_MINIMAL,delimiter=',')
+    if type(listname)==list:
+        for item in listname:
+            wr.writerow([item])
+    elif type(listname)==np.ndarray:
+        if len(listname.shape)==1:
+            for item in listname:
+                wr.writerow([item])
+        else:
+            for item in listname:
+                wr.writerow(item)
+    else:
+        print("the structure you are writing is neither a list nor an np.ndarray")
+		
+    runnumberfile.close()
 #########################################################################################################
 #######################################   INITIALIZING        ###########################################
 #########################################################################################################
 file_path="C://Users//Mary//Music//Documents//Python Scripts//Try_20150516_portion_of_each_segment//output.csv"
-
+complete_path_to_save_figure="C://Users//Mary//Music//Documents//Python Scripts//Try_20150516_portion_of_each_segment//four_categories_two_features.png"
+complete_path_to_save_figure2="C://Users//Mary//Music//Documents//Python Scripts//Try_20150516_portion_of_each_segment//four_categories_six_features.png"
+complete_path_to_save_csv="C://Users//Mary//Music//Documents//Python Scripts//Try_20150516_portion_of_each_segment//category_features.csv"
 #########################################################################################################
 #######################################   MAIN PROGRAM        ###########################################
 #########################################################################################################
 tstart = time.time()
 X=get_matrix_from_csv(file_path)
+Y=np.zeros((len(X),2))
+Y[:,0]=X[:,0]
+Y[:,1]=X[:,3]
 
 kmeans=KMeans(n_clusters=4)
 kmeans.fit(X)
@@ -61,6 +89,19 @@ colors=["g.","r.","y.","k."]
 for i in range(len(X)):
     plt.plot(X[i][0],X[i][3],colors[labels[i]],markersize=10)
     
-plt.scatter(centroids[:,0],centroids[:,3])
+plt.scatter(centroids[:,0],centroids[:,1])
+
+
+plt.show
+#plt.savefig(complete_path_to_save_figure)
+plt.savefig(complete_path_to_save_figure2)
+plt.clf()
+
+yes_certain_category=(labels==1)
+new_Y=Y[yes_certain_category]
+plt.scatter(new_Y[:,0],new_Y[:,1])
+
+category_zero_list=np.array(yes_certain_category+0,dtype='int')
+write_array_to_csv(complete_path_to_save_csv,category_zero_list)
 
 print('RUN TIME: %.2f secs' % (time.time()-tstart))
