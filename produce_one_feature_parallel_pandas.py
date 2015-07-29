920b2c8c42ae5a41e2cb4c6c43eae7fe1cee8d62
Yu-Ru Huang <mary@linshunghuang.com>
2015-07-29 15:47:34 +0800

    small fixes

--- produce_one_feature_parallel_pandas.py
+++ produce_one_feature_parallel_pandas.py
@@ -18,9 +18,7 @@ Created on Fri June 30
 import os #-------------------------------------------------------Miscellaneous operating system interfaces
 import numpy as np #----------------------------------------------array manipulation, scientific computing
 import csv#-------------------------------------------------------read write csv files
-import matplotlib.pyplot as plt  #--------------------------------John Hunter's  2D plotting library
 import re #-------------------------------------------------------regular expressions
-from matplotlib import rc 
 from joblib import Parallel, delayed
 import multiprocessing
 import time
@@ -41,25 +39,6 @@ def ensure_dir(f):
     d=os.path.abspath(f)
     if not os.path.exists(d):
         os.makedirs(d)
-   
-
-def read_single_variable_as_stringlist_csv(csvpathfilename, variablename):
-#   import csv
-#   import numpy as np      
-    
-    notfirst=1
-    thelist=[]
-    
-    with open(csvpathfilename,'rU') as csvfile:
-        contents=csv.reader(csvfile)
-        for row in contents:
-            if notfirst==1:
-               whichcolumn=row.index(variablename)
-               notfirst+=1
-            else:
-               thelist.append(float(row[(whichcolumn)]))
-        
-    return np.array(thelist)  
     
 def extract_serial_number(filename):
 #    import re
@@ -88,10 +67,7 @@ def write_array_to_csv(filename_path,listname):
         print("the structure you are writing is neither a list nor an np.ndarray")
 		
     runnumberfile.close()
-    
 
-  
-    
 #######################################
 
 
@@ -105,11 +81,9 @@ folder_to_read_from="E://MovedFromD//CSV//TS1//FiveFeatures_2492runs//setpoint"#
 folder_to_read_from2="E://MovedFromD//CSV//TS1//FiveFeatures_2492runs//current"#--------------------------------------------"folder to access"
 folder_to_read_from3="E://MovedFromD//CSV//TS1//FiveFeatures_2492runs//deviation"
 path_to_save_list="C://Users//A30123.ITRI//Documents//Python Scripts//New_for_event_mining//Try_20150729_PLA_five_features//TS1_GapSpeed_variance.csv"#----------------------------"folder to save output to"
-
 #########################################################################################################
 #######################################   MAIN PROGRAM        ###########################################
 #########################################################################################################
-
 tstart = time.time()
 files_in_folder = os.listdir(folder_to_read_from) 
 files_in_folder.sort(key=extract_serial_number)
@@ -127,10 +101,6 @@ def repeat_this(i,files_in_folder,folder_to_read_from,folder_to_read_from2,folde
     single_file_path2=os.path.join(folder_to_read_from2, temp_file_name.replace('-setpoint','-current'))
     #single_file_path3=os.path.join(folder_to_read_from3, temp_file_name.replace('-setpoint','-deviation'))
 
-    #--------------------------------------------------------------------------------reads values from csv file of specified sensor variable
-#    setpoint_values=read_single_variable_as_stringlist_csv(single_file_path,sensor_variables)
-#    current_values=read_single_variable_as_stringlist_csv(single_file_path2,sensor_variables)
-#    deviation_values=read_single_variable_as_stringlist_csv(single_file_path3,sensor_variables)
     #All=pd.read_csv(single_file_path)
     #setpoint_values=np.asarray(All[:][sensor_variables])
     
